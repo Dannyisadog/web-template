9f082a99da29efc2940d4e53dd5a7aa14119065c
DannyChen <dannyisadog10@gmail.com>
2023-01-21 12:10:08 +0800

    feat: add credential signin method

--- components/Signin/index.tsx
+++ components/Signin/index.tsx
@@ -3,36 +3,69 @@ import { signIn } from "next-auth/react";
 import style from "./Signin.module.css";
 import { useSession } from "next-auth/react"
 import { useEffect } from "react";
+import Link from "next/link";
+import { useRef, useState } from "react";
 
 const SigninPage = (props: SigninProps) => {
   const { providers } = props;
   const { status } = useSession();
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (status === "authenticated") {
-      location.href = "/";
+      location.href = "/signin/done";
     }
-  }, []);
+  }, [status]);
+
+  const signin = async (provider: any) => {
+    if (provider === 'credentials') {
+      const result = await signIn(provider, {
+        callbackUrl: '/signin/done',
+        redirect: false,
+        email: emailRef.current?.value,
+        password: passwordRef.current?.value
+      })
+      
+      const { error } = result;
+      setErrorMsg(error);      
+    } else {
+      signIn(provider, {
+        callbackUrl: '/signin/done',
+      })
+    }
+  }
 
   return (
-    <div className={style.signin_page} >
-      <div className={style.container}>
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button
-              className={style.signin_button}
-              onClick={() => {
-                signIn(provider.id, {
-                  callbackUrl: '/signin/done'
-                })
-              }}
-            >
-              Sign in with {provider.name}
-            </button>
+    <>
+      <div className={style.signin_page} >
+        <div className={style.credential_container}>
+          {errorMsg && <div className={style.err_msg}>{errorMsg}</div>}
+          <input ref={emailRef} className={style.input} placeholder="email" type="text" />
+          <input ref={passwordRef} className={style.input} placeholder="password" type="password" />
+          <div className={style.register_container}>
+            <Link href="/reset/password" className={style.register_link}>Forgot Password</Link>
+            <Link href="/register" className={style.register_link}>Register</Link>
           </div>
-        ))}
+        </div>
+        <div className={style.container}>
+          {Object.values(providers).map((provider) => (
+            <div className={style.signin_button_container} key={provider.name}>
+              <button
+                className={style.signin_button}
+                onClick={() => {
+                  signin(provider.id)
+                }}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
