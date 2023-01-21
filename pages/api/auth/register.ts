c0278f8631b23672e460efda4dbd46418c6915c6
DannyChen <dannyisadog10@gmail.com>
2023-01-21 12:03:35 +0800

    feat: add register api

---
+++ pages/api/auth/register.ts
@@ -0,0 +1,69 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { isEmail } from "utils/util";
+import { create as createUser, findUserByEmail } from "../service/User";
+import bcrypt from "bcryptjs";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.status(405).send({
+      "error": "post method allowed"
+    });
+  }
+  const body = JSON.parse(req.body)
+  const {
+    username,
+    email,
+    password,
+    passwordConfirm
+  } = body;
+
+  try {
+    if (!username) {
+      throw new Error("username should not be empty");
+    }
+
+    if (!isEmail(email)) {
+      throw new Error("email is invalid");
+    }
+
+    const existUser = await findUserByEmail(email);
+
+    if (existUser) {
+      throw new Error("user existed");
+    }
+
+    if (password !== passwordConfirm) {
+      throw new Error("password should be the same");
+    }
+
+    if (password.length < 8) {
+      throw new Error("password length should more than 8 characters");
+    }
+
+    const passwordHash = bcrypt.hashSync(password, 10);
+
+    const newUser = {
+      name: username,
+      password: passwordHash,
+      email,
+      image: ""
+    };
+
+    await createUser(newUser);
+
+    res.status(200).send({
+      "success": "register successfully"
+    });
+    
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(200).send({
+        "error": e.message
+      });  
+    } else {
+      res.status(200).send({
+        "error": "something's crash"
+      });  
+    }
+  }
+}
