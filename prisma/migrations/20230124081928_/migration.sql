ec2915a3314504a759fd9fc6bb9c580f60f51018
DannyChen <dannyisadog10@gmail.com>
2023-01-25 10:35:01 +0800

    build: generate migration with user table

---
+++ prisma/migrations/20230124081928_/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "image" TEXT,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
