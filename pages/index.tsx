c6f9bbbddeccf1c83aaa2f84190484be7f5cd677
DannyChen <dannyisadog10@gmail.com>
2023-01-27 21:11:12 +0800

    feat: remove header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,30 +8,6 @@ export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
         <div className={styles.center}>
           <Image
             className={styles.logo}
